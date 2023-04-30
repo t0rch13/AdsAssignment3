f8e40f2a2bb446bd61566b4fb52204a395401439
t0rch13 <batyrhan22.11@gmail.com>
2023-05-01 00:04:00 +0600

    feat: MyHashNode class inside MyHashTable

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -1,2 +1,18 @@
-public class MyHashTable {
+public class MyHashTable <K, V> {
+    private class MyHashNode<K, V>{
+        private K key;
+        private V value;
+        private MyHashNode<K, V> next;
+        public  MyHashNode (K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public String toString(){
+            return "{ " + key + " " + value + " }";
+        }
+    }
+
+
 }
